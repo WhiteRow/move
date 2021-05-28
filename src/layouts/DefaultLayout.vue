383689121d4268ad3a394748a2b404329c25b8f9
EgorB <egor.belousov@Q.local>
2021-05-28 09:55:43 +0300

    feat: сделал дефолтный лейаут

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="page">
+    <header>header</header>
+    <slot />
+  </main>
+</template>
+<script>
+const defaultLayout = 'DefaultLayout';
+
+export default {
+  name: 'DefaultLayout',
+
+  computed: {
+    layout() {
+      const layout = this.$route.meta.layout || defaultLayout;
+      return () => import(`@/layouts/${layout}.vue`);
+    },
+  },
+};
+</script>
